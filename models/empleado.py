a7ef1983ec54d4a91e2d03ed4566f339f198a787
usuario <adriigil99@gmail.com>
2020-11-15 13:02:13 +0100

    Empleado e Instalacion

--- models/empleado.py
+++ models/empleado.py
@@ -6,3 +6,9 @@ from odoo import models, fields, api
 class Empleado(models.Model):
      _name = 'uposports.empleado'
      _description = 'uposports Empleado'
+
+     dni = fields.Char(string="DNI", required=True, help="DNI del empleado")
+     nombre = fields.Char(string="Nombre", required=True, help="Nombre del empleado")
+     apellidos = fields.Char(string="Apellidos", required=True, help="Apellidos del empleado")
+     telefono = fields.Integer("Telefono", required=True)
+
--- models/reserva.py
+++ models/reserva.py
@@ -6,3 +6,8 @@ from odoo import models, fields, api
 class Reserva(models.Model):
      _name = 'uposports.reserva'
      _description = 'uposports Reserva'
+
+     id_reserva = fields.Integer("ID Reserva")
+     freserva = fields.Datetime('Fecha de reserva', required=True, autodate = True)
+     horaInicio= fields.Datetime('Hora de inicio', required=True, autodate = True)
+     horaFin = fields.Datetime('Hora de fin', required=True, autodate = True)
