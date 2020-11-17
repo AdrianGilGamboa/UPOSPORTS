75a1ba347ea50355c9e4e3e4091f5e9d024411b3
usuario <adriigil99@gmail.com>
2020-11-17 17:20:43 +0100

    ¿Modulo complejo deportivo?

--- models/complejodeportivo.py
+++ models/complejodeportivo.py
@@ -7,5 +7,7 @@ class ComplejoDeportivo(models.Model):
      _name = 'uposports.complejodeportivo'
      _description = 'uposports ComplejoDeportivo'
 
+     nombre = fields.Char('Nombre', size=30, required=True)
+
      abono_id=fields.One2many('uposports.abono',"complejoDeportivo_id","Abonos")
      reserva_id=fields.One2many('uposports.reserva',"complejoDeportivo_id","Reservas del cliente")
